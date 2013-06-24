07fa694d69f90bfddef674518d46729093fd3e6e
khairil.yusof@gmail.com <khairi.yusof@gmail.com>
2013-06-24 13:28:25 +0800

    Change copyright year to 2012-2013. Fixes #6.

--- wp-content/themes/esplanade/footer.php
+++ wp-content/themes/esplanade/footer.php
@@ -12,7 +12,7 @@
 			</div>
               <div style="float:right;margin-right:2%;"> <img src="<?php bloginfo('template_directory');?>/images/canadian_government_logo_small.png" alt="Canada"></div>
               <div class="clear"></div>
-              <p><a href="http://www.digital-review.org/">link to DIRAP website</a> &nbsp;&nbsp;&nbsp;&nbsp;Orbicom and IDRC - 2012</p>		
+              <p><a href="http://www.digital-review.org/">link to DIRAP website</a> &nbsp;&nbsp;&nbsp;&nbsp;Orbicom and IDRC 2012-2013</p>		
 			</div><!-- #copyright -->
           
 		</div><!-- #footer -->    
